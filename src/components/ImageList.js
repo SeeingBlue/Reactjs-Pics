bf5605ad702f8a0c973d6fa3820fe62f72762805
SeeingBlue <seeingblue@seeingblue.us>
2021-02-03 09:57:13 -0500

    added image card and finished tutorial

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,12 @@
+import './ImageList.css'
 import React from 'react'
+import ImageCard from './ImageCard'
 
 const ImageList=(props)=>{
-    const images = props.images.map(({description, alt_description, id, urls}) => {
-        return <img alt={description == null ? alt_description : description} key={id} src={urls.regular}></img>
+    const images = props.images.map((image) => {
+        return <ImageCard key={image.id} image={image} />
     })
-    return <div>{images}</div>
+    return <div className="image-list">{images}</div>
 
 }
 
